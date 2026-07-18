7e0b42afc79bb347c9e3432c01ac91481add15fb
agilPrasetyo <agil40728@gmail.com>
2026-07-18 18:13:46 +0700

    chore: remove vercel build output from git tracking

--- .vercel/output/_functions/chunks/compiler_Dkxe-Ujf.mjs
+++
@@ -1,2 +0,0 @@
-import "./server_DCOFL32C.mjs";
-export {};
--- .vercel/output/_functions/chunks/supabase_kTRWeqky.mjs
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-var supabase = createClient("https://xpoddtzxsopwzojycmwx.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhwb2RkdHp4c29wd3pvanljbXd4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MzY4MTIxMSwiZXhwIjoyMDk5MjU3MjExfQ.JLUA8DnaCpIYmdgCb7990yBKgKXQ0uJveZ8hd2893Mo", { auth: {
-	autoRefreshToken: false,
-	persistSession: false
-} });
-//#endregion
-export { supabase as t };
